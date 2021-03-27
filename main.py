f941149c0a55fd1eeb61f5161945b1956f7a9646
sauravmittal001 <sauravmittal001@gmail.com>
2021-03-27 15:59:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import csv
 import sys
 
 sys.setrecursionlimit(1000000)
-# key: cost, value: number of sequences (cycles*10)
-m = {}
-cost_matrix = []
-visited = [False] * 10
+m = {}                                  # key: cost, value: number of sequences (cycles*10)          
+cost_matrix = []                        #Used to store individual digit transistion cost
+visited = [False] * 10                  #Used to all generate possible permutations
 
 
 def calculate_cost(arr):
     c = 0
     n = len(arr)
     for i in range(n):
-        c += cost_matrix[arr[i % n]][arr[(i + 1) % n]]
-    return c
+        c += cost_matrix[arr[i % n]][arr[(i + 1) % n]] 
+    return c                            #cumulative cost for jumping to next element in arr
 
 
-def permutations(A, i, n, seq):
-    if i == n:
+def permutations(A, i, n, seq):         #To calculate all possible orders of digital number change
+    if i == n:                          #covering all numbers from 0 to 9 only once 
         cost = calculate_cost(seq)
         if cost in m.keys():
-            m[cost] += 1
-        else:
+            m[cost] += 1                #In m dictionary, we store number of permutations which yield
+        else:                           #that particular cost, to get count as well as max/min cost
             m[cost] = 1
         return
 
@@ -50,7 +49,9 @@ def print_cost_matrix():
         [1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 0, 1, 1]
     ]
-
+    #This is being implmented using the digital logic concept of 7-segment dispaly and which some LEDs
+    #out of 7 glow to show a digit. Wherever a difference in a bit(LED) is found, cost is incremeted
+    
     for i in range(0, 10):
         num1 = nums[i]
         for j in range(0, 10):
@@ -62,10 +63,9 @@ def print_cost_matrix():
             cost_matrix[i][j] = cost
             cost_matrix[j][i] = cost
 
-    for i in range(0, 10):
-        print(cost_matrix[i])
-
-
+    for i in range(0, 10):              #Printing the complete cost matrix, to get transition cost 
+        print(cost_matrix[i])           #from any node(digit) to any node(digit)
+                                        
 def print_csv():
     print("Cost.csv file generated!")
     with open('cost.csv', 'w', newline='') as file:
@@ -75,20 +75,20 @@ def print_csv():
     print("\n")
 
 
-def evaluate_cycle_cost(n):
-    print("\n")
+def evaluate_cycle_cost(n):             #To prove that cost is same over a loop in a cyclic order
+    print("\n")                         #for the required sequence
     arr = [None] * n
     for i in range(n):
         for j in range(n):
-            arr[j] = (j + i) % n
+            arr[j] = (j + i) % n        #Loops in same order(cycle), changing start points 
         print(arr, ": cost = ", calculate_cost(arr))
     print("For the given cycles, all costs are equal!\n")
 
 
 def print_best_worst():
     best_cost = -1
-    worst_cost = 71 # cost cannot cross 70
-    for i in m.keys():
+    worst_cost = 71                     # cost cannot cross 70
+    for i in m.keys():                  #Lookup the dictionary and find the mini and max cost for a sequence
         best_cost = max(i, best_cost)
         worst_cost = min(i, worst_cost)
     print("Best cost =", best_cost, ", Worst cost =", worst_cost, "\n")
@@ -96,9 +96,9 @@ def print_best_worst():
 
 def print_cycle_count():
     print("Cost, Number of cycles")
-    # every cycle will be repeated 10 times in permutation
-    for i in m.keys():
-        print(i, ",", int(m[i] / 10))
+                                        # every cycle will be repeated 10 times in permutation
+    for key in sorted(m):
+        print(key, int(m[key] / 10))
 
 
 if __name__ == '__main__':
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     print_cost_matrix()
     A = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     seq = [None] * 10
-    # number of permutations = 3628800
+    # number of permutations = 3628800 calc by 10!
     permutations(A, 0, len(A), seq)
 
     # Stage D: Evaluating the Cost of a Counting Cycle
